94ad3318c69f0c25b88e267906ad844f52335b16
renanwilson <renan.alves.ra16@gmail.com>
2023-01-12 11:48:51 -0300

    feat: chage default image in index page

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,10 +1,22 @@
 import { Header } from "Components/Header/Index";
 import { Menu } from "Components/Menu/Index";
-import banner from "assets/banner.png";
+
 import styles from "./Index.module.scss";
 import { GalleryWithNowPlaying } from "Components/Gallery/GalleryNowPlaying";
+import { api } from "services/api";
+import { useEffect, useState } from "react";
+import { AiFillStar } from "react-icons/ai";
 
 export function Index() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    api.get("movie/popular").then((resp) => {
+      setData(resp.data.results[1]);
+    });
+  }, [setData]);
+
+  const urlImages = "https://image.tmdb.org/t/p/t/p/w1920_and_h800_multi_faces";
+
   return (
     <>
       <Header />
@@ -13,8 +25,14 @@ export function Index() {
           <Menu />
 
           <div className={styles.principal__image}>
-            <h1>A galeria mais completa do espaco</h1>
-            <img src={banner} alt="A imagem da terra vista do espaco" />
+            <img
+              src={`${urlImages}/${data.backdrop_path}`}
+              alt="A imagem da terra vista do espaco"
+            />
+            <h1>{data.title}</h1>
+            <h2>
+              <AiFillStar color="#D4AF37" /> {data.vote_average}
+            </h2>
           </div>
         </section>
         <div className={styles.gallery}>
